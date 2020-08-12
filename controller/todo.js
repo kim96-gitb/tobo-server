3e3fe186b861fa51dd2a9c10ef71cd815f44e033
kim96-gitb <67768245+kim96-gitb@users.noreply.github.com>
2020-08-12 10:32:20 +0900

    완료여부 체크

--- controller/todo.js
+++ controller/todo.js
@@ -9,9 +9,33 @@ exports.Alltodo = async (req, res, next) => {
   let limit = req.query.limit;
   let query = `select * from todo limit ${offset},${limit}`;
 
+  if (!offset || !limit) {
+    res.status(500).json({ success: false, error: "경로가 이상합니다" });
+  }
+
   try {
     [rows] = await connection.query(query);
     res.status(200).json({ success: true, msg: rows, cnt: rows.length });
+  } catch (e) {
+    res.status(400).json({ success: false, error: e });
+  }
+};
+// @desc 완료 여부 체크 및 해제
+// @routes PUT api/v1/todo/update
+// @reqeust
+// @response success
+exports.updatetodo = async (req, res, next) => {
+  let id = req.body.id;
+  let completed = req.body.completed;
+
+  if (!id || !completed) {
+    res.status(500).json({ success: false, error: "정보입력을 해주세요" });
+  }
+
+  let query = `update todo set completed = ${completed} where id = ${id}`;
+  try {
+    [result] = await connection.query(query);
+    res.status(200).json({ success: true, msg: result });
   } catch (e) {
     res.status(400).json({ success: false, error: e });
   }
--- routes/todo.js
+++ routes/todo.js
@@ -1,8 +1,8 @@
 const express = require("express");
-const { Alltodo } = require("../controller/todo");
+const { Alltodo, updatetodo } = require("../controller/todo");
 
 const router = express.Router();
 
 router.route("/").get(Alltodo);
-
+router.route("/update").put(updatetodo);
 module.exports = router;
